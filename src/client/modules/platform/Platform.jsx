f6a422c6590cd8d80955277097bbf4258605bd6e
Alex <30322223+Infrared96@users.noreply.github.com>
2019-09-17 23:42:39 +0300

    create react components

---
+++ src/client/modules/platform/Platform.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Platform extends Component {
+    render() {
+        return (
+            <div>PLATFORM</div>
+        );
+    }
+}
